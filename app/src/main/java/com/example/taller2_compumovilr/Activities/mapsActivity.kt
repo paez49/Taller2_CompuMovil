9c5796f613a299a9312080b7baddbaf2824b928c
noUsoGitHub <89752129+noUsoGitHub@users.noreply.github.com>
2023-04-21 04:03:31 -0500

    Demasiado
    Falta implementar el stalkeo y el bono

--- app/src/main/java/com/example/taller2_compumovilr/Activities/mapsActivity.kt
+++ app/src/main/java/com/example/taller2_compumovilr/Activities/mapsActivity.kt
@@ -2,16 +2,27 @@ package com.example.taller2_compumovilr.Activities
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.taller2_compumovilr.R
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,6 +30,7 @@ import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.AutocompleteSessionToken
@@ -27,11 +39,16 @@ import com.google.android.libraries.places.api.model.PlaceLikelihood
 import com.google.android.libraries.places.api.net.FetchPlaceRequest
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.*
 
 
-class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonClickListener,GoogleMap.OnMyLocationClickListener{
+class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonClickListener,GoogleMap.OnMyLocationClickListener,GoogleMap.OnMapLongClickListener{
     private lateinit var map:GoogleMap
+    private var sensorManager: SensorManager? = null
+    private var lightSensor: Sensor? = null
+    private var maxValue = 0f
+    private var state = 1
     companion object{
         const val REQUEST_CODE_LOCATION = 303
     }
@@ -40,8 +57,8 @@ class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
             Places.initialize(applicationContext, getString(com.example.taller2_compumovilr.R.string.google_maps_key), Locale.US);
         }
         super.onCreate(savedInstanceState)
+        //Revisar
         setContentView(com.example.taller2_compumovilr.R.layout.activity_maps)
-
         val buscarSitio = findViewById<AutoCompleteTextView>(com.example.taller2_compumovilr.R.id.buscarSitio)
         buscarSitio.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH && v.text.toString().isNotEmpty()) {
@@ -52,8 +69,9 @@ class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
                 return@OnEditorActionListener true
             }
             false
-        })
-
+        })//Revisar
+        val myButton = findViewById<FloatingActionButton>(R.id.stalk)
+        myButton.setOnClickListener(::stalkear)
 
         createFragment()
     }
@@ -66,7 +84,23 @@ class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
         map=p0
         map.setOnMyLocationButtonClickListener(this)
         map.setOnMyLocationClickListener(this)
+        map.setOnMapLongClickListener(this)
         enableLocation()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+        sensorManager!!.registerListener(object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                val lightValue = event.values[0]
+                if (lightValue < 1000) {
+                    map.setMapStyle(MapStyleOptions(resources
+                        .getString(R.string.style_dark)));
+                } else {
+                    map.setMapStyle(MapStyleOptions(resources
+                        .getString(R.string.style_light)));
+                }
+            }
+            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+        }, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
     private fun buscarLugar(query: String) {
 
@@ -86,7 +120,7 @@ class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
                         val fields = listOf(Place.Field.NAME, Place.Field.LAT_LNG)
                         val request = FetchPlaceRequest.builder(placeId, fields).build()
                         placesClient.fetchPlace(request).addOnSuccessListener { response ->
-                        val place = response.place
+                            val place = response.place
                             print(place.latLng)
                             if(place.latLng!= null && place.name != null) {
                                 Toast.makeText(this, place.name, Toast.LENGTH_SHORT).show()
@@ -111,7 +145,64 @@ class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
         )
 
     }
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this,"Centrando en tu ubicación actual",Toast.LENGTH_SHORT).show()
+        return false
+    }
+    @SuppressLint("MissingPermission")
+    override fun onMyLocationClick(p0: Location) {
+        val placeFields: List<Place.Field> = listOf(Place.Field.NAME)
+        val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED) {
+            val placesClient = Places.createClient(this)
+            val placeResponse = placesClient.findCurrentPlace(request)
+            placeResponse.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val response = task.result
+                    val placeLikelihood: PlaceLikelihood =response.placeLikelihoods[0]
+                    var msg = "Te encuentras cerca de ${placeLikelihood.place.name}"
+                    Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
 
+                } else {
+                    val exception = task.exception
+                    if (exception is ApiException) {
+
+                    }
+                }
+            }
+        } else {
+            requestLocationPermission()
+        }
+
+
+    }
+    override fun onMapLongClick(latLng: LatLng) {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+        val address = addresses?.get(0)?.getAddressLine(0)
+        val markerOptions = MarkerOptions().position(latLng).title(address)
+        map.addMarker(markerOptions)
+    }
+
+    //Botones
+
+    private fun stalkear(view: View) {
+        val button = view as FloatingActionButton
+        if (state==2) {
+            button.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.blue)));
+            button.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_play))
+            state = (1)
+        }
+        else
+        {
+            button.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.red)));
+            button.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_pause))
+            state = 2
+        }
+    }
+
+    //Permisos
     private fun isLocationPermisionGranted() = ContextCompat.checkSelfPermission(
             this,Manifest.permission.ACCESS_FINE_LOCATION
         )== PackageManager.PERMISSION_GRANTED
@@ -139,12 +230,8 @@ class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 REQUEST_CODE_LOCATION)
     }
-
     @SuppressLint("MissingSuperCall", "MissingPermission")
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,grantResults: IntArray
     ) {
         when(requestCode){
             REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
@@ -157,7 +244,6 @@ class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
             else ->{}
         }
     }
-
     @SuppressLint("MissingPermission")
     override fun onResumeFragments() {
         super.onResumeFragments()
@@ -169,43 +255,5 @@ class mapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
         }
     }
 
-    override fun onMyLocationButtonClick(): Boolean {
-        Toast.makeText(this,"Centrando en tu ubicación actual",Toast.LENGTH_SHORT).show()
-        return false
-    }
-
-    @SuppressLint("MissingPermission")
-    override fun onMyLocationClick(p0: Location) {
-
-
-// Use fields to define the data types to return.
-        val placeFields: List<Place.Field> = listOf(Place.Field.NAME)
-// Use the builder to create a FindCurrentPlaceRequest.
-        val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
-// Call findCurrentPlace and handle the response (first check that the user has granted permission).
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-            PackageManager.PERMISSION_GRANTED) {
-            val placesClient = Places.createClient(this)
-            val placeResponse = placesClient.findCurrentPlace(request)
-            placeResponse.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val response = task.result
-                    val placeLikelihood: PlaceLikelihood =response.placeLikelihoods[0]
-                    var msg = "Te encuentras cerca de ${placeLikelihood.place.name}"
-                    Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
-
-                } else {
-                    val exception = task.exception
-                    if (exception is ApiException) {
-
-                    }
-                }
-            }
-        } else {
-            requestLocationPermission()
-        }
-
-
-    }
 
 }
